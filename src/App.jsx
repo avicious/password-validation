d5d18e8425fcc1741e399300ca62661abde58c21
Shubham Kumar <shubham.kr2951@gmail.com>
2026-02-15 20:36:16 +0530

    app working

--- src/App.jsx
+++ src/App.jsx
@@ -14,8 +14,72 @@ const App = () => {
     confirmPassword: "",
   });
 
+  const handleUserInput = (name, value) => {
+    setFormInput({
+      ...formInput,
+      [name]: value,
+    });
+  };
+
   const validateFormInput = (event) => {
     event.preventDefault();
+
+    let inputError = {
+      email: "",
+      password: "",
+      confirmPassword: "",
+    };
+
+    if (!formInput.email && !formInput.password) {
+      setFormError({
+        ...inputError,
+        email: "Enter a valid email address",
+        password: "Password should not be empty",
+      });
+      return;
+    }
+
+    if (!formInput.email) {
+      setFormError({
+        ...inputError,
+        email: "Enter a valid email address",
+      });
+      setFormInput((prevState) => ({
+        ...prevState,
+        successMsg: "",
+      }));
+      return;
+    }
+
+    if (!formInput.password) {
+      setFormError({
+        ...inputError,
+        password: "Password should not be empty",
+      });
+      setFormInput((prevState) => ({
+        ...prevState,
+        successMsg: "",
+      }));
+      return;
+    }
+
+    if (formInput.password !== formInput.confirmPassword) {
+      setFormError({
+        ...inputError,
+        confirmPassword: "Password and confirm password should be the same",
+      });
+      setFormInput((prevState) => ({
+        ...prevState,
+        successMsg: "",
+      }));
+      return;
+    }
+
+    setFormError(inputError);
+    setFormInput((prevState) => ({
+      ...prevState,
+      successMsg: "Validation Successful",
+    }));
   };
 
   return (
@@ -33,7 +97,9 @@ const App = () => {
             <input
               type="text"
               value={formInput.email}
-              onChange={({ target }) => {}}
+              onChange={({ target }) => {
+                handleUserInput(target.name, target.value);
+              }}
               name="email"
               id="email"
               className="input"
@@ -47,7 +113,9 @@ const App = () => {
             <input
               type="password"
               value={formInput.password}
-              onChange={({ target }) => {}}
+              onChange={({ target }) => {
+                handleUserInput(target.name, target.value);
+              }}
               name="password"
               id="password"
               className="input"
@@ -61,8 +129,10 @@ const App = () => {
             <input
               type="text"
               value={formInput.confirmPassword}
-              onChange={({ target }) => {}}
-              name="confirm-password"
+              onChange={({ target }) => {
+                handleUserInput(target.name, target.value);
+              }}
+              name="confirmPassword"
               id="confirm-password"
               className="input"
               placeholder="Confirm Password"
